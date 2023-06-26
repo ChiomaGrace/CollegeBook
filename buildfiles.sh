73cbfbdea1013702fc3cbc731e3f3c8dd3681ea9
Chioma Ubogagu <chioma.ubogagu@simpleviewinc.com>
2023-06-26 13:47:26 -0700

    deploying to vercel

--- buildfiles.sh
+++ buildfiles.sh
@@ -1,4 +1,15 @@
-echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+# echo "BUILD START"
+#  python3.9 -m pip install -r requirements.txt
+#  python3.9 manage.py collectstatic --noinput --clear
+#  echo "BUILD END"
+
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
+
