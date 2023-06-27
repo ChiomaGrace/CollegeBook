3f80341d68cc889fb1b1c8761a7b0c2572428025
Chioma Ubogagu <chioma.ubogagu@simpleviewinc.com>
2023-06-27 02:26:28 -0700

    deploying to vercel

--- wall/settings.py
+++ wall/settings.py
@@ -55,8 +55,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent #Use this for local
 SECRET_KEY = '29st2j=m_g=qpxaerv#q9j%9*e7!vo4!u79(f$6q@-6jg7a1+h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True #Turned off for deployment
-DEBUG = False 
+# DEBUG = #for locally
+DEBUG = False #for deployment
 
 ALLOWED_HOSTS = [
     # '.vercel.app', 
@@ -245,22 +245,24 @@ STATIC_URL = '/static/' #This url is how a client or browser can access static f
 
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-    # os.path.join(BASE_DIR, "wallApp/static"), #baseDir references the "home" so for me starting at collegebook
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "wallApp/static"), #baseDir references the "home" so for me starting at collegebook
     # os.path.join(BASE_DIR, "wallApp/static/CSS"), #baseDir references the "home" so for me starting at collegebook
     # os.path.join(BASE_DIR, "wallApp/static/Images"),
     # os.path.join(BASE_DIR, "wallApp/static/JavaScript"),
-# )
+)
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # #I added the below lines of code for deployment #
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/') # contains the absolute path to the file system where media files will be uploaded to store the images on the computer.
 # MEDIA_ROOT= os.path.join(BASE_DIR, '/media/') # contains the absolute path to the file system where media files will be uploaded to store the images on the computer.
 MEDIA_URL= "/media/"  #is the reference URL for browser to access the files over Http.
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #This enables the app to now serve static assets directly from Gunicorn in production
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'#This enables the app to now serve static assets directly from Gunicorn in production
 
 # The below code solves the collectstatic multiple paths error
 STATICFILES_FINDERS = (
